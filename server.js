7c084a49562f19f3b780587fa03f547b3a5a4f57
Melanie DeWitt <melaniebd1986@gmail.com>
2021-08-11 17:56:20 -0600

    added node modules and skeleton code

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const { Counter } = require("@smiirl/smiirl-library-js");
 const express = require('express');
-
+const port = 2005;
 const app = express();
+
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
+});
+
+
+app.get('/', (req, res) => {
+  res.send({number:42})
+});
