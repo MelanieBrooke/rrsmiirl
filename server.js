a42fa66f2a1d54f98a9eae57c9cfc4d8063812a8
Melanie DeWitt <melaniebd1986@gmail.com>
2021-08-20 19:52:54 -0600

    app functional, calling youtube API and returning followers

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const { Counter } = require("@smiirl/smiirl-library-js");
 const express = require('express');
 const port = 2005;
+const dotenv = require('dotenv');
 const app = express();
+const fetch = require('node-fetch');
+
+dotenv.config();
+
+let youtubeAPIKey = process.env.YOUTUBE_API_KEY;
+let channelID = process.env.YOUTUBE_CHANNEL_ID;
+let youtubeURL = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelID}&key=${youtubeAPIKey}`;
+
 
 app.listen(port, () => {
   console.log(`listening at port ${port}`);
@@ -9,5 +18,17 @@ app.listen(port, () => {
 
 
 app.get('/', (req, res) => {
-  res.send({number:42})
+  fetch(youtubeURL)
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    return data.items[0].statistics.subscriberCount
+  })
+  .then(count => {
+    res.send({number:count})
+  })
+  .catch(err => {
+    res.send(err)
+  })
 });
